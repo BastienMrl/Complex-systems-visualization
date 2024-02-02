5bcccfd867a4f02b17e1ee8d378f022a1802dde0
Victor Barrey <Vickybarrey@outlook.fr>
2024-02-02 15:17:02 +0100

    config model db

--- complexSystemViewer/viewer/models.py
+++ complexSystemViewer/viewer/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class ConfiguationItem(models.Model):
+    name = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.name
+    
+class Parameter(models.Model):
+    name = models.CharField(max_length=128)
+    configuration = models.ForeignKey(ConfiguationItem, on_delete=models.CASCADE)
+    
+    class Meta:
+        abstract = True
+        
+class IntRangeParameter(Parameter):
+    minDefaultValue = models.IntegerField()
+    maxDefaultValue = models.IntegerField()
