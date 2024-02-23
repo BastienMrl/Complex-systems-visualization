d3e1bb9f556bf8592d1aa55faa7a472d61ec60a6
jmoze <jmoze@etu.u-bordeaux.fr>
2024-02-23 12:31:17 +0100

    remake tools list

--- complexSystemViewer/viewer/views.py
+++ complexSystemViewer/viewer/views.py
@@ -17,7 +17,7 @@ def index(request):
     for t in transformers:
         param = Parameter.objects.filter(configurationItem=t.id).select_subclasses()
         transformersParam[t] = param
-    return render(request, "index.html", {"model":modelSelected , "modelsName":modelsName, "rulesParameters":rulesParameters, "transformers":transformersParam}) 
+    return render(request, "index.html", {"model":modelSelected , "modelsName":modelsName, "rulesParameters":rulesParameters, "transformers":transformersParam, "toolsList":toolsList}) 
 
 def addTransformer(request, modelsName, transformerType ):
     model = ALifeModel.objects.filter(name=modelsName).first()
